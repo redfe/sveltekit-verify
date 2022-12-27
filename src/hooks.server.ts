1b48fa78904ed045df4450040e50e6f4ed9f07cb
redfe <34601358+redfe@users.noreply.github.com>
2022-12-27 12:24:10 +0000

    認証情報の取得方法を修正

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,12 +6,13 @@ export const handle = (async ({ event, resolve }) => {
 		event.locals.user = JSON.parse(env.DEV_USER);
 	} else {
 		const header = event.request.headers.get('x-ms-client-principal');
-		console.log('x-ms-client-principal', header);
+		//console.log('x-ms-client-principal', header);
 		if (header) {
 			const encoded = Buffer.from(header ?? '', 'base64');
 			const decoded = encoded.toString('ascii');
-			console.log('decoded', decoded);
-			event.locals.user = JSON.parse(decoded);
+			//console.log('decoded', decoded);
+			const user: any = JSON.parse(decoded);
+			event.locals.user = { id: user.id, name: user.userDetails };
 		}
 	}
 
