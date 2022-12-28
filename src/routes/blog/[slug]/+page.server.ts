b06068b6b1738b658604baef959f8a925aebce70
redfe <34601358+redfe@users.noreply.github.com>
2022-12-28 03:37:18 +0000

    修正

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -2,8 +2,8 @@ import type { PageServerLoad } from './$types';
 import * as db from '$lib/server/database';
 import { error } from '@sveltejs/kit';
 
-export const load = (async ({ params }) => {
-	const posts = await db.getAll();
+export const load = (async ({ params, locals }) => {
+	const posts = await db.getAll(locals.user.id);
 	const post = posts.find((post) => post.slug === params.slug);
 	if (!post) throw error(404);
 	return { post };
