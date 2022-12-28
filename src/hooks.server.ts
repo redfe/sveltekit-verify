b53c74203663e9fa8884d4d1fb91ae4a850b8781
redfe <34601358+redfe@users.noreply.github.com>
2022-12-28 06:48:29 +0000

    デバッグログ修正

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,20 +2,21 @@ import type { Handle } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 
 export const handle = (async ({ event, resolve }) => {
+	event.locals.user = { id: '', name: '' };
 	if (env.DEV_USER) {
 		event.locals.user = JSON.parse(env.DEV_USER);
 	} else {
 		const header = event.request.headers.get('x-ms-client-principal');
-		//console.log('x-ms-client-principal', header);
+		//console.log('### x-ms-client-principal', header);
 		if (header) {
 			const encoded = Buffer.from(header ?? '', 'base64');
 			const decoded = encoded.toString('ascii');
-			console.log('decoded', decoded);
+			console.log('### decoded', decoded);
 			const user: any = JSON.parse(decoded);
 			event.locals.user = { id: user.userId, name: user.userDetails };
-			console.log('user', event.locals.user);
 		}
 	}
+	console.log('### user at hook', event.locals.user);
 
 	const response = await resolve(event);
 	return response;
