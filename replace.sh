1bb2487d93b136d1494715bb0b0014845bf78095
redfe <tetsu999@gmail.com>
2023-01-01 23:45:42 +0900

    AbortSignal2に変更してみた

--- replace.sh
+++ replace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat ./build/server/sk_render/index.js | sed 's/return !!(proto && proto.constructor.name === "AbortSignal");/console.log("###",{signal:signal,proto:proto});\r&/g' > temp
+cat ./build/server/sk_render/index.js | sed 's/return !!(proto && proto.constructor.name === "AbortSignal");/return !!(proto \&\& proto.constructor.name === "AbortSignal2");/g' > temp
 mv temp ./build/server/sk_render/index.js
