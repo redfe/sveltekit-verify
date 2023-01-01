e03531a48d9a6798867ec399da91ec5ef97c2786
redfe <tetsu999@gmail.com>
2023-01-01 18:40:25 +0900

    AbortSignalのエラー対応

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	esbuild: {
+		// "Expected signal to be an instanceof AbortSignal" 対応。see: Expected signal to be an instanceof AbortSignal"
+		keepNames: true
 	}
 };
 
