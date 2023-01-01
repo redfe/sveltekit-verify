1cf24f1e0667b114f4a924ce8ba6fd81b98ceec4
redfe <tetsu999@gmail.com>
2023-01-01 18:53:50 +0900

    Revert "AbortSignalのエラー対応"
    This reverts commit e03531a48d9a6798867ec399da91ec5ef97c2786.

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	esbuild: {
-		// "Expected signal to be an instanceof AbortSignal" 対応。see: Expected signal to be an instanceof AbortSignal"
-		keepNames: true
 	}
 };
 
