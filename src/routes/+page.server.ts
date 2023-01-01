86a1e447306ca78728a2e4d8df524307a4ea24b7
redfe <tetsu999@gmail.com>
2023-01-01 15:52:06 +0900

    修正

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,13 @@ import { main } from './storage-demo';
 import { fail } from '@sveltejs/kit';
 
 export const actions = {
-	runBlobDemo: async ({ request, locals }) => {
+	runBlobDemo: async ({}) => {
+		console.log('### start blob demo!');
 		try {
 			await main();
 			return { message: 'success!' };
 		} catch (error: any) {
+			console.error(error);
 			return fail(500, { message: 'error:' + error.message });
 		}
 	}
