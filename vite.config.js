475fc39d126eae68a567374779ce83f7e86b5b2c
redfe <34601358+redfe@users.noreply.github.com>
2023-02-17 02:26:54 +0000

    AbortSignalエラー対策思案

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,24 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	// Expected signal to be an instanceof AbortSignal 対策の試し
+	// https://zenn.dev/merutin/articles/0a69e136a746e1
+	build: {
+		minify: 'terser',
+		terserOptions: {
+			// https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+			mangle: false,
+			sourceMap: true,
+			// compress: false,
+			// for airtable
+			keep_classnames: /AbortSignal/,
+			keep_fnames: /AbortSignal/,
+			output: {
+				beautify: true,
+				indent_level: 1
+			}
+		}
 	}
 };
 
